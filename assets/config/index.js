47e1ff045fe636fefbf4deb385ae653fdf387894
Carlangasv <carloszzzxc@hotmail.com>
2020-05-17 21:39:58 -0500

    Implementación api

--- assets/config/index.js
+++ assets/config/index.js
@@ -1,5 +1,13 @@
+let url = "http://127.0.0.1:8000/api/v1/"
+
+if(process.env.NODE_ENV=="production"){
+    url = process.env.NODE_URL
+}else{
+    url = "http://127.0.0.1:8000/api/v1/"
+}
+
 const config = {
-    url_api: "http://127.0.0.1:8000/api/v1/"
+    url_api: url
 }
 
 module.exports = config;
