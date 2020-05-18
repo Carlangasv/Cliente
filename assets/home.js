8f90d1bb27c2bf6f33069fd1e717e7048a4ef0e3
Carlangasv <carloszzzxc@hotmail.com>
2020-05-18 10:12:28 -0500

    Cambios

--- assets/home.js
+++ assets/home.js
@@ -51,31 +51,36 @@ export default {
       let id = localStorage.getItem("id");
       let url = config.url_api + "permisos/";
       let token = localStorage.getItem("token");
+      let arrayPermisos = []
       axios
         .get(url + id, {
           headers: { token },
         })
         .then((response) => {
           console.log("Permisos");
-          console.log(response.data.info);
           let array = response.data.info;
           for (let i in array) {
             let temp = {
               Accion: "",
               Url: "",
+              Modulo: "",
+              Proyecto:"",
+              Opcion:""
             };
             temp.Modulo = array[i].Modulo;
             temp.Proyecto = array[i].Proyecto;
             temp.Url = array[i].Url;
+            temp.Accion = array[i].Acción;
+            temp.Opcion = array[i].Opción;
             if (temp.Proyecto === "Publicaciones") {
               this.lista_opciones.push(temp);
             } else if (temp.Proyecto === "Convenios y Movilidad") {
               this.lista_modulos.push(temp);
             }
           }
-          console.log("lista modulos");
+          console.log("Todos movilidad");
           console.log(this.lista_modulos);
-          console.log("lista opciones");
+          console.log("Todos publicaciones");
           console.log(this.lista_opciones);
         })
         .catch((error) => {
@@ -106,9 +111,9 @@ export default {
               this.lista_mov_acc.push(temp);
             }
             }
-            console.log("lista publicaciones");
+            console.log("Permisos publicaciones");
             console.log(this.list_prop_acc);
-            console.log("lista movilidad");
+            console.log("Permisos movilidad");
             console.log(this.lista_mov_acc);
         })
         .catch((error) => {
