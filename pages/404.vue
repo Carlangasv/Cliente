acc905e1091f5cb61e0fe06b37723e1db28e76f6
Carlangasv <carloszzzxc@hotmail.com>
2020-05-19 17:25:59 -0500

    404 page

---
+++ pages/404.vue
@@ -0,0 +1,12 @@
+<template>
+<b-container>
+           <b-img
+          src="../images/404.png"
+          fluid
+          alt="Fluid image"
+        ></b-img>
+        
+</b-container>
+</template>
+<style scoped>
+
